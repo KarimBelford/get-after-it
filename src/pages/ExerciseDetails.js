fc8d13c3badff61a82369743af110204f542b30d
Karim Belford <71955017+KarimBelford@users.noreply.github.com>
2023-07-05 21:34:19 -0400

    created exercise details components

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,12 +1,20 @@
 import React, {useEffect,useState} from 'react'
-import Pagination from '@mui/material/Pagination'
-import {Box, Stack, Typography} from '@mui/material'
+import { useParams } from 'react-router-dom'
+import {Box} from '@mui/material'
 
-import { exerciseOptions,fetchData } from '../utils/fetchData'
+import {exerciseOptions,fetchData} from '../utils/fetchData'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+
 
 const ExerciseDetails = () => {
   return (
-   <div>ExerciseDetails</div>
+   <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+   </Box>
   )
 }
 
